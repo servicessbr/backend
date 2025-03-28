01dfdd7b9a35931a544b567f324049177f4cb570
Marco Túlio <comunidadeservicess@gmail.com>
2025-03-28 15:20:44 -0300

    ...

--- public/keys/servicess-6e07b-firebase-adminsdk-fbsvc-da0ab15083.js
+++ public/keys/servicess-6e07b-firebase-adminsdk-fbsvc-da0ab15083.js
@@ -4,7 +4,7 @@ module.exports = {
   "type": "service_account",
   "project_id": "servicess-6e07b",
   "private_key_id": process.env.STORAGE_PRIVATE_KEY_ID,
-  "private_key": process.env.STORAGE_PRIVATE_KEY,
+  "private_key": `-----BEGIN PRIVATE KEY-----\n${process.env.STORAGE_PRIVATE_KEY}\n-----END PRIVATE KEY-----\n`,
   "client_email": "firebase-adminsdk-fbsvc@servicess-6e07b.iam.gserviceaccount.com",
   "client_id": "108863560407963818909",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
