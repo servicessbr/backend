205f00c1344356797c4752fba593ff087be0700d
Marco Túlio <comunidadeservicess@gmail.com>
2025-04-18 16:32:22 -0300

    fix

--- src/email/transporter.js
+++ src/email/transporter.js
@@ -3,44 +3,27 @@ const nodemailer = require('nodemailer');
 
 var mailConfig;
 
-if (process.env.NODE_ENV) {
-    // all emails are catched by ethereal.email
-    mailConfig = {
-        host: 'smtp.ethereal.email',
-        secure: true,
-        secureConnection: false,
-        tls: {
-            ciphers: 'SSLv3'
-        },
-        requireTLS: true,
-        port: 465,
-        debug: true,
-        auth: {
-            user: 'suporte@servicess.com.br',
-            pass: process.env.MAIN_MAIL_PASSWORD
-        }
-    };
-} else {
-    // all emails are delivered to destination
-    mailConfig = {
-        host: "smtpout.secureserver.net",
-        secure: true,
-        /* 
-            * TLS requires secureConnection to be false
-        */
-        secureConnection: false,
-        tls: {
-            ciphers: 'SSLv3'
-        },
-        requireTLS: true,
-        port: 465,
-        debug: true,
-        auth: {
-            user: 'suporte@servicess.com.br',
-            pass: process.env.MAIN_MAIL_PASSWORD
-        }
-    };
-}
+
+// all emails are delivered to destination
+mailConfig = {
+    host: "smtpout.secureserver.net",
+    secure: true,
+    /* 
+        * TLS requires secureConnection to be false
+    */
+    secureConnection: false,
+    tls: {
+        ciphers: 'SSLv3'
+    },
+    requireTLS: true,
+    port: 465,
+    debug: true,
+    auth: {
+        user: 'suporte@servicess.com.br',
+        pass: process.env.MAIN_MAIL_PASSWORD
+    }
+};
+
 
 console.log(process.env.NODE_ENV, mailConfig);
 
