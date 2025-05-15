43d832065c42b1489aa179d99e923bbf560d1340
Marco Túlio <comunidadeservicess@gmail.com>
2025-05-15 16:54:15 -0300

    test 7 - paypal

--- src/controllers/paypalController.ts
+++ src/controllers/paypalController.ts
@@ -78,18 +78,18 @@ const paypalController = {
                                 description,
                                 quantity: 1,
                                 unit_amount: {
-                                    currency_code: 'USD',
+                                    currency_code: 'BRL',
                                     value: PRICE
                                 }
                             }
                         ],
 
                         amount: {
-                            currency_code: 'USD',
+                            currency_code: 'BRL',
                             value: PRICE,
                             breakdown: {
                                 item_total: {
-                                    currency_code: 'USD',
+                                    currency_code: 'BRL',
                                     value: PRICE
                                 }
                             }
